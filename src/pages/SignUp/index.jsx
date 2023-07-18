d612fe2795b0f56bc06f12e656025de4c001a53b
EduViera131 <eduviera369@gmail.com>
2023-07-18 16:27:56 -0300

    Create user function added

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,11 +1,39 @@
+import { useState } from 'react'
 import { Container, Form, Background } from './styles'
 import { FiMail, FiLock, FiUser } from 'react-icons/fi'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+
+import { api } from "../../services/api"
 
 import { Input } from '../../components/Input'
-import {Button} from '../../components/Button'
+import { Button } from '../../components/Button'
 
 export function SignUp() {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const navigate = useNavigate()
+
+  function handleSignUp() {
+    if (!name || !email || !password) {
+      return alert("Preencha todos os campos!")
+    }
+
+    api.post("/users", { name, email, password })
+      .then(() => {
+        alert("Usuário cadastrado com sucesso!")
+        navigate("/")
+      })
+      .catch(error => {
+        if (error.response) {
+          alert(error.response.data.message)
+        } else {
+          alert("Não foi possível cadastrar")
+        }
+      })
+  }
+
   return (
     <Container>
       <Background />
@@ -16,12 +44,12 @@ export function SignUp() {
 
         <h2>Crie sua conta</h2>
 
-        <Input type="text" placeholder="Nome" icon={FiUser} />
-        <Input type="text" placeholder="Seu e-mail" icon={FiMail} />
-        <Input type="password" placeholder="Senha" icon={FiLock} />
+        <Input type="text" placeholder="Nome" icon={FiUser} onChange={e => setName(e.target.value)} />
+        <Input type="text" placeholder="Seu e-mail" icon={FiMail} onChange={e => setEmail(e.target.value)} />
+        <Input type="password" placeholder="Senha" icon={FiLock} onChange={e => setPassword(e.target.value)} />
 
-        <Button title="Cadastrar" />
-        
+        <Button title="Cadastrar" onClick={handleSignUp} />
+
         <Link to="/"> Voltar para o login</Link>
       </Form>
 
