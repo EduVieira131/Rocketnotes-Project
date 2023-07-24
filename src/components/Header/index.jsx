7e64d2ec4caa1cb744c96c93f4c20fad11c9f6dd
EduViera131 <eduviera369@gmail.com>
2023-07-24 10:41:28 -0300

    :feat: Profile update function

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,19 +1,24 @@
 import { Container, Profile, Logout } from './styles'
 import { RiShutDownLine } from 'react-icons/ri'
+import avatarPlaceholder from '../../assets/avatar_placeholder.svg'
 
 import { useAuth } from '../../hooks/auth'
 
+import { api } from "../../services/api"
+
 export function Header() {
-  const { signOut } = useAuth()
+  const { signOut, user } = useAuth()
+
+  const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder
 
   return (
     <Container>
       <Profile to="/profile">
-        <img src="https://github.com/EduVieira131.png" alt="Foto do usuário" />
+        <img src={avatarUrl} alt={user.name} />
 
         <div>
           <span>Bem-vindo</span>
-          <strong>Eduardo Vieira</strong>
+          <strong>{user.name}</strong>
         </div>
       </Profile>
 
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,12 +1,49 @@
+import { useState } from "react"
+
 import { Container, Form, Avatar } from './styles'
 import { FiArrowLeft, FiCamera, FiLock, FiMail, FiUser } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
 
+import { useAuth } from "../../hooks/auth"
+
+import { api } from "../../services/api"
+import avatarPlaceholder from "../../assets/avatar_placeholder.svg"
 
 import { Button } from '../../components/Button'
 import { Input } from '../../components/Input'
 
 export function Profile() {
+  const { user, updateProfile } = useAuth()
+
+  const [name, setName] = useState(user.name)
+  const [email, setEmail] = useState(user.email)
+  const [passwordOld, setPasswordOld] = useState()
+  const [passwordNew, setPasswordNew] = useState()
+
+  const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder
+  const [avatar, setAvatar] = useState(avatarUrl)
+  const [avatarFile, setAvatarFile] = useState(null)
+
+  async function handleUpdate() {
+    const user = {
+      name,
+      email,
+      password: passwordNew,
+      old_password: passwordOld
+    }
+
+    await updateProfile({ user, avatarFile })
+  }
+
+  function handleChangeAvatar(event) {
+    const file = event.target.files[0]
+    setAvatarFile(file)
+
+    const imagePreview = URL.createObjectURL(file)
+    setAvatar(imagePreview)
+
+  }
+
   return (
     <Container>
       <header>
@@ -18,22 +55,22 @@ export function Profile() {
       <Form>
         <Avatar>
           <img
-            src="https://github.com/EduVieira131.png"
+            src={avatar}
             alt="Foto do usuário"
           />
           <label htmlFor="avatar">
             <FiCamera />
 
-            <input type="file" id='avatar' />
+            <input type="file" id='avatar' onChange={handleChangeAvatar} />
           </label>
         </Avatar>
 
-        <Input placeholder="Nome" type="text" icon={FiUser} />
-        <Input placeholder="E-mail" type="text" icon={FiMail} />
-        <Input placeholder="Senha" type="password" icon={FiLock} />
-        <Input placeholder="Nova senha" type="password" icon={FiLock} />
+        <Input placeholder="Nome" type="text" icon={FiUser} value={name} onChange={e => setName(e.target.value)} />
+        <Input placeholder="E-mail" type="text" icon={FiMail} value={email} onChange={e => setEmail(e.target.value)} />
+        <Input placeholder="Senha" type="password" icon={FiLock} onChange={e => setPasswordOld(e.target.value)} />
+        <Input placeholder="Nova senha" type="password" icon={FiLock} onChange={e => setPasswordNew(e.target.value)} />
 
-        <Button title="Salvar" />
+        <Button title="Salvar" onClick={handleUpdate} />
       </Form>
     </Container>
   )
