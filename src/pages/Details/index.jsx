da592cf7d262f30d3708d013b377f31044a4bcbf
EduViera131 <eduviera369@gmail.com>
2023-07-26 13:59:55 -0300

    :feat: Delete note function created

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -21,6 +21,16 @@ export function Details() {
     navigate("/")
   }
 
+  async function handleRemove() {
+    const confirm = window.confirm("Deseja realmente remover a nota?")
+
+    if (confirm) {
+      await api.delete(`/notes/${params.id}`)
+    }
+    
+    navigate("/")
+  }
+
   useEffect(() => {
     async function fetchNote() {
       const response = await api.get(`/notes/${params.id}`)
@@ -39,7 +49,7 @@ export function Details() {
 
         <main>
           <Content>
-            <ButtonText title="Excluir nota" />
+            <ButtonText title="Excluir nota" onClick={handleRemove} />
 
             <h1>{data.title}</h1>
 
